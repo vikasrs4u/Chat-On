b526f7821738ac0609eb8c02855d71de46804070
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-21 18:53:31 -0700

    Made changes to handle signup

--- Chat On/Controller/SignUpViewController.swift
+++ Chat On/Controller/SignUpViewController.swift
@@ -7,31 +7,44 @@
 //
 
 import UIKit
+import Firebase
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController
+{
 
     @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var emailTextfield: UITextField!
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - SignUp
+    
+    //Method will be called when signup button is clicked.
+    @IBAction func signUpButtonClicked(_ sender: UIButton)
+    {
+        guard let email:String = emailTextfield.text else {return}
+        guard let password:String = passwordTextfield.text else {return}
+        
+        Auth.auth().createUser(withEmail: email, password: password)
+        { (authResult, error) in
+            if (error != nil)
+            {
+                print(error!)
+            }
+            else
+            {
+                
+                self.performSegue(withIdentifier:"goToChat" , sender: self)
+            }
+            
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
