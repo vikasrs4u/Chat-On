c527d2182c84978581d44703dc8a83e27d48a387
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-22 21:22:05 -0700

    Added constraints and maessage realted changes

--- Chat On/Controller/ChatViewController.swift
+++ Chat On/Controller/ChatViewController.swift
@@ -9,19 +9,60 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
 
-    @IBOutlet weak var messageTableView: UITableView!
-    @IBOutlet weak var sendButton: UIButton!
-    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var messageTableViews: UITableView!
     
-    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var sendButtonsOutlet: UIButton!
+    
+    @IBOutlet weak var heightConstraints: NSLayoutConstraint!
+    
+    @IBOutlet weak var messageTextfields: UITextField!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Since we have table view, we need to regsiter our class as delegate and data source for the table view
+        
+        messageTableViews.delegate = self
+        messageTableViews.dataSource = self
+        
+        // We need to regsiter the custom cell, below is the code for the same
+        messageTableViews.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
+        
+        // Method to resize the cell
+        configureTheTableViewCell()
+        
+    }
+    
+    
+    // Required Datasource methods for the table view
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell") as! CustomMessageCell
+        
+        let message = ["First Message","Seconddndfbdfbdfbdnfbdfndbfnbdnfdbfndbfnbdfbdfbndfbndbfndbfnbdfbdnbfndbfndbfndb Message","Third Message"]
+        
+        cell.messageBody.text = message[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    // If data in the cell is too long, then cell should resize itself
+    func configureTheTableViewCell()
+    {
+        messageTableViews.rowHeight = UITableViewAutomaticDimension
+        messageTableViews.estimatedRowHeight = 400.00
     }
 
     // This function is called when we click on logout button.
