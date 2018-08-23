dbe3c6e632208fb5497e80b3704f5df8c5765062
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-22 19:19:10 -0700

    Added login functionality

--- Chat On/Controller/LoginViewController.swift
+++ Chat On/Controller/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -14,26 +15,29 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextfield: UITextField!
     
     @IBOutlet weak var passwordTextfield: UITextField!
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginButtonClicked(_ sender: UIButton)
+    {
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!)
+        { (result, error) in
+            
+            if(error != nil)
+            {
+                print("Error in login")
+            }
+            else
+            {
+                self.performSegue(withIdentifier:"goToChat", sender: self)
+            }
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
