9a878ac76cf22f5285d12aecd5abcf3aa59e3acc
Vikas Shetty <vikasrs4u@gmail.com>
2018-08-30 13:45:32 -0700

    Added AlamofireImage pod file

--- Podfile
+++ Podfile
@@ -14,6 +14,7 @@ pod 'Firebase/Storage'
 pod 'Firebase/Core'
 pod 'SVProgressHUD'
 pod 'ChameleonFramework'
+pod 'AlamofireImage'
 
 end
 
--- Pods/Target Support Files/Pods-Chat On/Pods-Chat On-frameworks.sh
+++ Pods/Target Support Files/Pods-Chat On/Pods-Chat On-frameworks.sh
@@ -143,6 +143,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
@@ -151,6 +153,8 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AlamofireImage/AlamofireImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
