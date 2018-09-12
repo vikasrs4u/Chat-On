88e415931e3fc2182f34a5306dfdb63effbe6c91
Vikas Shetty <vikasrs4u@gmail.com>
2018-09-11 20:00:15 -0700

    Added watchkit extension and added app icon.
    Now notification in watch will display app icon as well

---
+++ Chat On Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Chat On Watch Extension
+//
+//  Created by Vikas R S on 9/11/18.
+//  Copyright © 2018 Vikas Radhakrishna Shetty. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
